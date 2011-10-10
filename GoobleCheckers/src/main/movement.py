87cff333e9e870ec815f809bd6e7a94c18583732
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2011-10-10 03:47:36 +0000

    refactoring test & movement - movement only works for one piece now.

--- GoobleCheckers/src/main/movement.py
+++ GoobleCheckers/src/main/movement.py
@@ -6,35 +6,37 @@ Created on 2011-10-03
 
 class Movement(object):
 
-    def __init__(self, board):
+    def __init__(self, board, row, col):
         self.board = board
+        self.row = row
+        self.col = col
+        self.piece = board.get_piece(row, col)
+        if self.piece is not None:
+            self.origin = self.piece.get_origin()
         
-    def get_available_moves(self, row, col):
-        if self.board.invalid_position(row, col):
+    def get_available_moves(self):
+        if self.piece is None:
             return []
         
-        piece = self.board.get_piece(row, col)
-        if piece is None:
-            return []
-        
-        origin = piece.get_origin()
         king_moves = []
-        if piece.is_king():
-            king_moves = self._get_moves_for_king(row, col, origin) 
+        if self.piece.is_king():
+            king_moves = self._add_moves_for_col(self.row - self.origin.value, -1, -self.origin.value)\
+                       + self._add_moves_for_col(self.row - self.origin.value, +1, -self.origin.value)
         
-        return king_moves + self._add_moves_for_col(row + origin.value, col, -1, origin.value, origin, piece.is_king())\
-             + self._add_moves_for_col(row + origin.value, col, +1, origin.value, origin, piece.is_king())
+        return king_moves\
+             + self._add_moves_for_col(self.row + self.origin.value, -1, self.origin.value)\
+             + self._add_moves_for_col(self.row + self.origin.value, +1, self.origin.value)
         
-    def _add_moves_for_col(self, row, col, col_dir, row_dir, origin, king):
-        new_col = col + col_dir
+    def _add_moves_for_col(self, row, col_dir, row_dir):
+        new_col = self.col + col_dir
         if self.board.valid_position(row, new_col) and self.board.get_piece(row, new_col) is None:
             return [[(row, new_col)]]
-        elif self.board.get_piece(row, new_col) is not None and self.board.get_piece(row, new_col).get_origin() != origin:
-            return self._start_recursion(row, new_col, col_dir, row_dir, origin, king)
+        elif self.board.get_piece(row, new_col) is not None and self.board.get_piece(row, new_col).get_origin() != self.origin:
+            return self._start_recursion(row, new_col, col_dir, row_dir)
             
         return []
         
-    def _start_recursion(self, row, col, col_dir, row_dir, origin, king):
+    def _start_recursion(self, row, col, col_dir, row_dir):
         '''
         We have the row & column on which there is a piece. we have to keep going
         along the diagonal -> if the next piece is occupied, return empty list - this isn't a valid move
@@ -53,26 +55,21 @@ class Movement(object):
         
         '''Keep going along the same direction'''
         new_new_row = new_row + row_dir
-        self._recurse(new_new_row, new_col, -1, row_dir, move, moves, origin, king)
-        self._recurse(new_new_row, new_col, +1, row_dir, move, moves, origin, king)
+        self._recurse(new_new_row, new_col, -1, row_dir, move, moves)
+        self._recurse(new_new_row, new_col, +1, row_dir, move, moves)
         
         
         '''but if it's a king, it can change direction along the vertical...'''
-        if king:
+        if self.piece.is_king():
             new_new_row = new_row - row_dir
-            self._recurse(new_new_row, new_col, col_dir, -row_dir, move, moves, origin, king)
+            self._recurse(new_new_row, new_col, col_dir, -row_dir, move, moves)
         
         return moves
     
-    def _recurse(self, row, col, col_dir, row_dir, move, moves, origin, king):
-        if self.board.get_piece(row, col + col_dir) is not None and self.board.get_piece(row, col + col_dir).get_origin() != origin:
-            new_moves = self._start_recursion(row, col + col_dir, col_dir, row_dir, origin, king)
+    def _recurse(self, row, col, col_dir, row_dir, move, moves):
+        if self.board.get_piece(row, col + col_dir) is not None and self.board.get_piece(row, col + col_dir).get_origin() != self.origin:
+            new_moves = self._start_recursion(row, col + col_dir, col_dir, row_dir)
             if len(new_moves) > 0 and move in moves:
                 moves.remove(move)
             [moves.append(move + m) for m in new_moves]
-            
-    def _get_moves_for_king(self, row, col, origin):
-        return self._add_moves_for_col(row - origin.value, col, -1, -origin.value, origin, True)\
-             + self._add_moves_for_col(row - origin.value, col, +1, -origin.value, origin, True)
-        
         
--- GoobleCheckers/src/test/movementtest.py
+++ GoobleCheckers/src/test/movementtest.py
@@ -10,32 +10,34 @@ from main.movement import Movement
 
 class MovementCalculatorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.calc = Movement(Board())
-
     def test_get_available_moves_from_location_with_no_piece(self):
-        moves = self.calc.get_available_moves(0, 1)
+        calc = Movement(Board(), 0, 1)
+        moves = calc.get_available_moves()
         self.assertEqual(len(moves), 0)
         
     def test_get_avail_moves_from_location_with_piece_that_can_move_from_top(self):
-        moves = self.calc.get_available_moves(2, 2);
+        calc = Movement(Board(), 2, 2)
+        moves = calc.get_available_moves();
         self.assertEqual(len(moves), 2)
         self.assertEqual([(3, 1)], moves[0])
         self.assertEqual([(3, 3)], moves[1])
         
     def test_get_avail_moves_from_location_with_piece_that_can_move_from_bottom(self):
-        moves = self.calc.get_available_moves(5, 1)
+        calc = Movement(Board(), 5, 1)
+        moves = calc.get_available_moves()
         self.assertEqual(len(moves), 2)
         self.assertEqual([(4, 0)], moves[0])
         self.assertEqual([(4, 2)], moves[1])
         
     def test_get_avail_moves_from_corner_location_from_bottom_gives_only_one_valid_move(self):
-        moves = self.calc.get_available_moves(5, 7)
+        calc = Movement(Board(), 5, 7)
+        moves = calc.get_available_moves()
         self.assertEqual(len(moves), 1)
         self.assertEqual([(4, 6)], moves[0])
         
     def test_get_avail_moves_from_corner_location_from_top_gives_only_one_valid_move(self):
-        moves = self.calc.get_available_moves(2, 0)
+        calc = Movement(Board(), 2, 0)
+        moves = calc.get_available_moves()
         self.assertEqual(len(moves), 1)
         self.assertEqual([(3, 1)], moves[0])
         
