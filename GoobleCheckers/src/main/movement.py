9ec64aedb8af3d1f325e0b42c7fc6f584cd97b61
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2011-10-10 03:54:34 +0000

    more refactoring: get rid of row parameter

--- GoobleCheckers/src/main/movement.py
+++ GoobleCheckers/src/main/movement.py
@@ -20,23 +20,24 @@ class Movement(object):
         
         king_moves = []
         if self.piece.is_king():
-            king_moves = self._add_moves_for_col(self.row - self.origin.value, -1, -self.origin.value)\
-                       + self._add_moves_for_col(self.row - self.origin.value, +1, -self.origin.value)
+            king_moves = self._add_moves_for_col(-self.origin.value, -1)\
+                       + self._add_moves_for_col(-self.origin.value, +1)
         
         return king_moves\
-             + self._add_moves_for_col(self.row + self.origin.value, -1, self.origin.value)\
-             + self._add_moves_for_col(self.row + self.origin.value, +1, self.origin.value)
+             + self._add_moves_for_col(self.origin.value, -1)\
+             + self._add_moves_for_col(self.origin.value, +1)
         
-    def _add_moves_for_col(self, row, col_dir, row_dir):
+    def _add_moves_for_col(self, row_dir, col_dir):
+        new_row = self.row + row_dir
         new_col = self.col + col_dir
-        if self.board.valid_position(row, new_col) and self.board.get_piece(row, new_col) is None:
-            return [[(row, new_col)]]
-        elif self.board.get_piece(row, new_col) is not None and self.board.get_piece(row, new_col).get_origin() != self.origin:
-            return self._start_recursion(row, new_col, col_dir, row_dir)
+        if self.board.valid_position(new_row, new_col) and self.board.get_piece(new_row, new_col) is None:
+            return [[(new_row, new_col)]]
+        elif self.board.get_piece(new_row, new_col) is not None and self.board.get_piece(new_row, new_col).get_origin() != self.origin:
+            return self._start_recursion(new_row, new_col, row_dir, col_dir)
             
         return []
         
-    def _start_recursion(self, row, col, col_dir, row_dir):
+    def _start_recursion(self, row, col, row_dir, col_dir):
         '''
         We have the row & column on which there is a piece. we have to keep going
         along the diagonal -> if the next piece is occupied, return empty list - this isn't a valid move
@@ -55,20 +56,20 @@ class Movement(object):
         
         '''Keep going along the same direction'''
         new_new_row = new_row + row_dir
-        self._recurse(new_new_row, new_col, -1, row_dir, move, moves)
-        self._recurse(new_new_row, new_col, +1, row_dir, move, moves)
+        self._recurse(new_new_row, new_col, row_dir, -1, move, moves)
+        self._recurse(new_new_row, new_col, row_dir, +1, move, moves)
         
         
         '''but if it's a king, it can change direction along the vertical...'''
         if self.piece.is_king():
             new_new_row = new_row - row_dir
-            self._recurse(new_new_row, new_col, col_dir, -row_dir, move, moves)
+            self._recurse(new_new_row, new_col, -row_dir, col_dir, move, moves)
         
         return moves
     
-    def _recurse(self, row, col, col_dir, row_dir, move, moves):
+    def _recurse(self, row, col, row_dir, col_dir, move, moves):
         if self.board.get_piece(row, col + col_dir) is not None and self.board.get_piece(row, col + col_dir).get_origin() != self.origin:
-            new_moves = self._start_recursion(row, col + col_dir, col_dir, row_dir)
+            new_moves = self._start_recursion(row, col + col_dir, row_dir, col_dir)
             if len(new_moves) > 0 and move in moves:
                 moves.remove(move)
             [moves.append(move + m) for m in new_moves]
