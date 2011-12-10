736fc623ccc5c62a104c6b4762658d7ccbe72314
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2011-12-10 21:39:34 +0000

    add test to make sure if there's no moves for some piece, it doesn't get added to the all moves list

--- GoobleCheckers/src/test/ai/originmovestest.py
+++ GoobleCheckers/src/test/ai/originmovestest.py
@@ -7,27 +7,13 @@ import unittest
 from main.game import origin
 from test.util.testboard import TestBoard
 from test.util.testcase import as_move_list
-import main.game.movements.movement as movement
-
-class OriginMoves(object):
-    
-    def __init__(self, board):
-        self.board = board
-        
-    def get_moves_for(self, origin):
-        all_moves = []
-        for row in range(0, self.board.DEFAULT_HEIGHT):
-            for col in range(0, self.board.DEFAULT_WIDTH):
-                if self.board.get_piece(row, col) is not None and self.board.get_piece(row, col).get_origin() == origin:
-                    all_moves.append(movement.Movement(self.board, row, col).get_available_moves())
-            
-        return all_moves
+from main.ai.originmoves import OriginMoves
 
 class OriginMovesTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.tboard = TestBoard()
-        
+    
     def test_get_all_moves_for_given_origin(self):
         # # # # # # # # # #
         #  0 1 2 3 4 5 6 7#
@@ -43,8 +29,9 @@ class OriginMovesTest(unittest.TestCase):
         self.tboard.place_piece(4, 0, origin.BOTTOM)
         self.tboard.place_piece(4, 3, origin.BOTTOM)
         self.tboard.place_piece(6, 5, origin.BOTTOM)
-        allmoves = OriginMoves(self.tboard.board).get_moves_for(origin.BOTTOM)
+        allmoves = OriginMoves(self.tboard.board, origin.BOTTOM).get_moves()
         
+        self.assertEqual(3, len(allmoves))
         self.assertEqual(1, len(allmoves[0]))
         self.assertEqual(as_move_list([(4, 0), (3, 1)]), allmoves[0][0])
         
@@ -52,9 +39,36 @@ class OriginMovesTest(unittest.TestCase):
         self.assertEqual(as_move_list([(4, 3), (3, 2)]), allmoves[1][0])
         self.assertEqual(as_move_list([(4, 3), (3, 4)]), allmoves[1][1])
         
-        self.assertEqual(2, len(allmoves[1]))
+        self.assertEqual(2, len(allmoves[2]))
         self.assertEqual(as_move_list([(6, 5), (5, 4)]), allmoves[2][0])
         self.assertEqual(as_move_list([(6, 5), (5, 6)]), allmoves[2][1])
+        
+    def test_origin_moves_list_contains_only_non_empty_move_lists(self):
+        # # # # # # # # # #
+        #  0 1 2 3 4 5 6 7#
+        #0 _ _ _ _ _ _ _ _#
+        #1 _ _ _ _ _ _ _ _#
+        #2 _ _ _ _ _ _ _ _#
+        #3 _ _ _ _ _ _ _ _#
+        #4 _ _ _ _ _ _ _ _#
+        #5 _ _ x _ x _ x _#
+        #6 _ _ _ B _ B _ _#
+        #7 _ _ _ _ B _ _ _#
+        # # # # # # # # # #
+        self.tboard.place_piece(6, 3, origin.BOTTOM)
+        self.tboard.place_piece(6, 5, origin.BOTTOM)
+        self.tboard.place_piece(7, 4, origin.BOTTOM)
+        
+        allmoves = OriginMoves(self.tboard.board,origin.BOTTOM).get_moves()
+        self.assertEqual(2, len(allmoves))
+
+        self.assertEqual(2, len(allmoves[0]))
+        self.assertEqual(as_move_list([(6, 3), (5, 2)]), allmoves[0][0])
+        self.assertEqual(as_move_list([(6, 3), (5, 4)]), allmoves[0][1])
+        
+        self.assertEqual(2, len(allmoves[1]))
+        self.assertEqual(as_move_list([(6, 5), (5, 4)]), allmoves[1][0])
+        self.assertEqual(as_move_list([(6, 5), (5, 6)]), allmoves[1][1])
 
 if __name__ == "__main__":
     unittest.main()
