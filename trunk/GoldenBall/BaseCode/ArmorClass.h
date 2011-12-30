bdf65448c1e63ddd47f351fbcd7247eef1e26a96
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2011-12-30 19:11:40 +0000

    Add "armor class" - there are three types; each mitigating different amounts of damage

---
+++ trunk/GoldenBall/BaseCode/ArmorClass.h
@@ -0,0 +1,14 @@
+#include "Attack.h"
+
+class ArmorClass{
+private:
+	double defenseRating;
+
+public:
+	ArmorClass(double defense);
+	int getMitigatedDamage(Attack attack);
+};
+
+const ArmorClass NO_ARMOR(0.0);
+const ArmorClass LOW_ARMOR(0.2);
+const ArmorClass HIGH_ARMOR(0.4);
