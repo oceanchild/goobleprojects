69748a7e0b23110da6756345f65920accb47783a
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-02-01 01:36:35 +0000

    rotation added - in rows and columns proper. full rotation of S shape checked.

---
+++ trunk/GoobleTetris/src/test/rotationtest.py
@@ -0,0 +1,41 @@
+'''
+Created on 2012-01-30
+
+@author: Gooble
+'''
+import unittest
+
+from main.point import Point
+from main.rotation import Rotation
+
+class Test(unittest.TestCase):
+
+
+    def test_rotate_s_shape_90_degs(self):
+        zero_deg_points = [Point(0, 0), 
+                           Point(0, 1), 
+                           Point(-1, 0), 
+                           Point(-1, -1)]
+        ninety_deg_points = [Point(0, 0), 
+                             Point(-1, 0), 
+                             Point(0, -1), 
+                             Point(1, -1)]
+        one_eighty_deg_points = [Point(0, 0),
+                                 Point(0, -1),
+                                 Point(1, 0),
+                                 Point(1, 1)]
+        two_seventy_deg_points = [Point(0, 0),
+                                  Point(1, 0),
+                                  Point(0, 1),
+                                  Point(-1, 1)]
+        self.assertEqual(ninety_deg_points, Rotation(zero_deg_points).get_new_points())
+        self.assertEqual(one_eighty_deg_points, Rotation(ninety_deg_points).get_new_points())
+        self.assertEqual(two_seventy_deg_points, Rotation(one_eighty_deg_points).get_new_points())
+        self.assertEqual(zero_deg_points, Rotation(two_seventy_deg_points).get_new_points())
+        
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_rotate_points']
+    unittest.main()
