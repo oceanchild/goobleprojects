6f7107b5b2f9cfabfc7376349f186c45cd8ee3b2
gooble.sk <gooble.sk@gmail.com>
2012-02-08 01:54:28 +0000

    finally a commit to Golden Ball. If i can't draw sprites, it's fine. I'll draw shapes until I can draw sprites.

--- GoldenBall/RunBaseCode/TestMain.cpp
+++ GoldenBall/RunBaseCode/TestMain.cpp
@@ -1,13 +1,29 @@
-#include <SFML/System.hpp>
+//#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main(){
-	sf::Clock Clock;
-	while(Clock.GetElapsedTime() < 5.f){
-		std::cout << Clock.GetElapsedTime() << std::endl;
-		std::cout << "Cake" << std::endl;
-		sf::Sleep(0.5f);
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Graphics");
+
+	sf::ConvexShape shape;
+	shape.SetPointCount(3);
+	shape.SetFillColor(sf::Color::Magenta);
+	shape.SetPoint(0, sf::Vector2f(10,10));
+	shape.SetPoint(1, sf::Vector2f(50, 10));
+	shape.SetPoint(2, sf::Vector2f(10, 50));
+
+	while (App.IsOpened()){
+		float ElapsedTime = App.GetFrameTime();
+
+		sf::Event Event;
+		if (App.PollEvent(Event)){
+			if (Event.Type == sf::Event::Closed)
+				App.Close();
+		}
+		App.Clear(sf::Color(200,0,0));
+		App.Draw(shape);
+		App.Display();
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
