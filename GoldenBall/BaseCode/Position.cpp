cc9964ea10411151809360192058d3304d4fcbb8
gooble.sk <gooble.sk@gmail.com>
2012-02-16 03:44:06 +0000

    Wow. finally got three test cases going RIGHT working. need to generalize for all other directions. I mean, finally, a thingy can move right without bumping into stuff.

--- GoldenBall/BaseCode/Position.cpp
+++ GoldenBall/BaseCode/Position.cpp
@@ -1,15 +1,48 @@
 #include "Position.h"
 
-Position::Position(int startX, int startY, int w, int h){
-	x = startX;
-	y = startY;
+Position::Position(Point tl, int w, int h){
+	topLeft = tl;
 	width = w;
 	height = h;
 }
 
+int Position::getX() const{
+	return topLeft.getX();
+}
+
+int Position::getY() const{
+	return topLeft.getY();
+}
+
+int Position::getEndX() const{
+	return getX() + width;
+}
+
+int Position::getEndY() const{
+	return getY() + height;
+}
+
+int Position::getWidth() const{
+	return width;
+}
+
+int Position::getHeight() const{
+	return height;
+}
+
 bool Position::overlaps(Position other){
-	return ((x <= other.x && other.x <= x + width) 
-			&& (y <= other.y && other.y <= y + height)) 
-			|| ((other.x <= x && x <= other.x + other.width) 
-			&& (other.y <= y && y <= other.y + other.height));
+	return ((getX() <= other.getX() && other.getX() <= getEndX()) 
+			&& (getY() <= other.getY() && other.getY() <= getEndY())) 
+			|| ((other.getX() <= getX() && getX() <= other.getEndX()) 
+			&& (other.getY() <= getY() && getY() <= other.getEndY()));
+}
+
+bool operator ==(const Position& lhs, const Position& rhs){
+	return lhs.getX() == rhs.getX() && lhs.getY() == rhs.getY()
+		&& lhs.getEndX() == rhs.getEndX() && lhs.getEndY() == rhs.getEndY();
+
+}
+
+bool operator !=(const Position& lhs, const Position& rhs){
+	return !(lhs == rhs);
 }
--- GoldenBall/BaseCode/Position.h
+++ GoldenBall/BaseCode/Position.h
@@ -1,11 +1,21 @@
+#include "Point.h"
+
 class Position{
 private:
 	int width;
 	int height;
-	int x;
-	int y;
+	Point topLeft;
 
 public:
-	Position(int startX, int startY, int w, int h);
+	Position(Point tl = Point(0, 0), int w = 0, int h = 0);
 	bool overlaps(Position other);
+	int getX() const;
+	int getY() const;
+	int getEndX() const;
+	int getEndY() const;
+	int getWidth() const;
+	int getHeight() const;
 };
+
+bool operator==(const Position& lhs, const Position& rhs);
+bool operator!=(const Position& lhs, const Position& rhs);
