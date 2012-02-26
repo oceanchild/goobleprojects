a6fa90cc0b3a18e49f819f5062f8089a8c7bc88e
gooble.sk <gooble.sk@gmail.com>
2012-02-26 19:57:59 +0000

    commit exe creator for tetris

---
+++ GoobleTetris/src/main/createexe.py
@@ -0,0 +1,13 @@
+'''
+Created on 2012-02-12
+
+@author: Gooble
+'''
+
+from cx_Freeze import setup, Executable
+
+setup(
+        name = "GoobleTetris",
+        version = "0.1",
+        description = "Tetris made by Gooble",
+        executables = [Executable("gamescreen.py")])
