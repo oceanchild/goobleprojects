8a113a90d1face4c3cb0f86b690ca10e2b6145f8
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-02-26 23:00:51 +0000

    move colours out of drawables

--- GoobleCheckers/src/main/view/drawables.py
+++ GoobleCheckers/src/main/view/drawables.py
@@ -3,40 +3,8 @@ Created on 2012-02-26
 
 @author: Gooble
 '''
-from main.game import origin
-
-RED = [255,0,0]
-BLACK = [0,0,0]
-WHITE = [255,255,255]
-BLUE = [0,0,255]
-
-class Drawable(object):
-    def __init__(self, x, y, width, height, colour):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.colour = colour
-        
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-    def __ne__(self, other):
-        return not self.__eq__(other)
-    def __repr__(self):
-        return "("+str(self.x)+", "+str(self.y)+")"
-
-class DrawablePiece(Drawable):
-    def draw(self, graphics, screen):
-        graphics.draw.ellipse(screen, self.colour, [self.x, self.y, self.width, self.height])
-
-class DrawableKing(DrawablePiece):
-    def draw(self, graphics, screen):
-        DrawablePiece.draw(self, graphics, screen)
-        graphics.draw.line(screen, WHITE, [self.x, self.y], [self.x+self.width, self.y+self.height])
-        
-class DrawableBackground(Drawable):
-    def draw(self, graphics, screen):
-        graphics.draw.rect(screen, self.colour, [self.x, self.y, self.width, self.height])
+from main.view.drawable import DrawableKing, DrawablePiece, DrawableBackground
+from main.view.colours import Colours
 
 class Drawables(object):
     
@@ -53,7 +21,7 @@ class Drawables(object):
         for row in range(0, self.game.get_num_rows()):
             for col in range(0, self.game.get_num_cols()):
                 piece = self.game.get_piece(row, col)
-                colour = self.get_colour_for(piece)
+                colour = Colours().piece(piece)
                 x = col * self.tile_width
                 y = row * self.tile_height
                 drawables.append(self.get_background(x, y, row, col))
@@ -67,18 +35,6 @@ class Drawables(object):
             drawables.append(held_drawable)
             
         return drawables
-        
-    def get_colour_for(self, piece):
-        if piece is not None and piece.get_origin() == origin.BOTTOM:
-            return RED
-        elif piece is not None:
-            return BLACK
-
-    def get_bg_colour_for(self, row, col):
-        if (row + col) % 2 == 0:
-            return WHITE
-        else:
-            return BLUE
         
     def get_held_drawable(self, position, piece, colour):
         x = int(position[0] - self.tile_width / 2)
@@ -98,5 +54,5 @@ class Drawables(object):
         return self.slotting.is_holding_piece(row, col) and position is not None
 
     def get_background(self, x, y, row, col):
-        bgcolour = self.get_bg_colour_for(row, col)
+        bgcolour = Colours().background(row, col)
         return DrawableBackground(x, y, self.tile_width, self.tile_height, bgcolour)
--- GoobleCheckers/src/test/view/drawablestest.py
+++ GoobleCheckers/src/test/view/drawablestest.py
@@ -4,41 +4,10 @@ Created on 2012-02-26
 @author: Gooble
 '''
 import unittest
-from main.view.drawables import Drawables, DrawableBackground, DrawablePiece,\
-    BLUE, WHITE, BLACK, DrawableKing, RED
-from main.game import origin
-from main.game.piece import Piece
-
-class MockGame(object):
-    def __init__(self):
-        top_king = Piece(origin.TOP)
-        top_king.set_king(True)
-        bottom_king = Piece(origin.BOTTOM)
-        bottom_king.set_king(True)
-        self.pieces = [[Piece(origin.TOP), None, top_king], 
-                       [Piece(origin.BOTTOM), None, bottom_king]]
-        
-    def get_tile_width(self):
-        return 10
-    def get_tile_height(self):
-        return 15
-    
-    def get_num_rows(self):
-        return len(self.pieces)
-    
-    def get_num_cols(self):
-        return len(self.pieces[0])
-    
-    def get_piece(self, row, col):
-        return self.pieces[row][col]
-
-class MockSlotting(object):
-    def __init__(self, row=None, col=None):
-        self.row = row
-        self.col = col
-    
-    def is_holding_piece(self, row, col):
-        return row == self.row and col == self.col
+from main.view.drawables import Drawables, DrawableBackground, DrawablePiece, DrawableKing
+from main.view.colours import WHITE,BLACK,RED,BLUE
+from test.view.mock.mockgame import MockGame
+from test.view.mock.mockslotting import MockSlotting
 
 class Test(unittest.TestCase):
 
