0d8a37a79b8a5b5520539d9f3a7d9002c278ccc6
gooble.sk <gooble.sk@gmail.com>
2012-03-10 22:29:36 +0000

    reduce duplication between rectangle & text by introducing drawable super class

--- GoobleTetris/src/main/display/drawing/text.py
+++ GoobleTetris/src/main/display/drawing/text.py
@@ -3,34 +3,18 @@ Created on 2012-03-10
 
 @author: Gooble
 '''
-class DrawableText(object):
+from main.display.drawing.drawable import Drawable
+
+class DrawableText(Drawable):
     def __init__(self, text, x, y):
+        Drawable.__init__(self, x, y)
         self.text = text
-        self.x = x
-        self.y = y
         
     def draw(self, screen):
         screen.blit(self.text, [self.x, self.y])
         
-    def contains(self, point):
-        pointx = point[0]
-        pointy = point[1]
-        return self.x <= pointx <= self.x + self.get_width() and self.y <= pointy <= self.y + self.get_height()
-    
     def get_width(self):
         return self.text.get_width()
     
     def get_height(self):
         return self.text.get_height()
-    
-    def set_x(self, x):
-        self.x = x
-        
-    def set_y(self, y):
-        self.y = y
-        
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
