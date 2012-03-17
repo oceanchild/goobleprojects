b0fd06ca93e4b78bdc4eb4fa6a9f7d21a0f5e7aa
gooble.sk <gooble.sk@gmail.com>
2012-03-17 21:16:43 +0000

    add the actual instructions to the how to play view

--- GoobleTetris/src/main/display/howto/view.py
+++ GoobleTetris/src/main/display/howto/view.py
@@ -18,12 +18,20 @@ class HowToPlayView(main.display.view.View):
         return main.display.drawing.factory.DrawableFactory().create_image(imagefile)
 
     def create_layout(self):
-        down = LabeledImage("downarrow.bmp", "Down")
+        down = LabeledImage("downarrow.bmp", "Press the Down Arrow Key to speed up the shape")
+        rotate = LabeledImage("uparrow.bmp", "Press the Up Arrow Key to rotate the shape")
+        left = LabeledImage("leftarrow.bmp", "Press the Left Arrow Key to move the shape left")
+        right = LabeledImage("rightarrow.bmp", "Press the Right Arrow Key to move the shape right")
+        spacebar = LabeledImage("spacebar.bmp", "Press the Space Bar to drop the shape") 
         backbutton = BackButton()
         
         layout = Layout()
-        layout.add_to_row(0, down)
-        layout.add_to_row(1, backbutton)
+        layout.add_to_row(0, left)
+        layout.add_to_row(1, right)
+        layout.add_to_row(2, down)
+        layout.add_to_row(3, rotate)
+        layout.add_to_row(4, spacebar)
+        layout.add_to_row(6, backbutton)
 
         self.buttons = [backbutton]
         self.layout = layout
