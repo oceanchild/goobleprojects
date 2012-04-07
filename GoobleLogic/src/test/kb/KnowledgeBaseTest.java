f092fef9c6182fa5f1d6c8d729e861b49202a6b3
gooble.sk <gooble.sk@gmail.com>
2012-04-07 18:43:31 +0000

    organization; remove all past work

--- GoobleLogic/src/test/kb/KnowledgeBaseTest.java
+++ GoobleLogic/src/test/kb/KnowledgeBaseTest.java
@@ -1,15 +1,17 @@
-package test;
+package test.kb;
 
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import main.kb.Constant;
 import main.kb.KnowledgeBase;
 import main.kb.Solution;
 import main.kb.Statement;
 import main.kb.Variable;
+import main.kb.stmts.GreaterThan;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,18 +36,20 @@ public class KnowledgeBaseTest {
    public void rule_and_statement_in_kb_query_provable_stmt_is_true() throws Exception{
       kb.addStatement(new Statement("smart", new Constant<String>("bob")));
       kb.addRule(new Statement("goodLooking", new Variable("X")), new Statement("smart", new Variable("X")));
-      assertTrue(kb.query(new Statement("goodLooking", new Constant<String>("bob"))));
-      assertFalse(kb.query(new Statement("goodLooking", new Constant<String>("joe"))));
+      
+      assertTrue(query("goodLooking", "bob"));
+      assertFalse(query("goodLooking", "joe"));
    }
-   
+
    @Test
    public void prove_statement_with_multiple_variables_and_use_of_numbers() throws Exception{
       // you're an adult if you're 18+
       kb.addStatement(new Statement("age", new Constant<String>("bob"), new Constant<Number>(17)));
       kb.addStatement(new Statement("age", new Constant<String>("joe"), new Constant<Number>(18)));
       kb.addRule(new Statement("adult", new Variable("X")), new Statement("age", new Variable("X"), new Variable("Y")), new GreaterThan(new Constant<Number>(17), new Variable("Y")));
-      assertFalse(kb.query(new Statement("adult", new Constant<String>("bob"))));
-      assertTrue(kb.query(new Statement("adult", new Constant<String>("joe"))));
+      
+      assertFalse(query("adult", "bob"));
+      assertTrue(query("adult", "joe"));
    }
    
    @Test
@@ -54,47 +58,11 @@ public class KnowledgeBaseTest {
       Solution solution = new Solution();
       solution.addVariableReplacement(new Variable("X"), new Constant<Number>(17));
       assertEquals(Arrays.asList(solution), kb.findSolutions(new Statement("age", new Constant<String>("bob"), new Variable("X"))));
+      assertEquals(Collections.<Solution>emptyList(), kb.findSolutions(new Statement("age", new Constant<String>("mary"), new Variable("X"))));
    }
-   
-   class GreaterThan extends Statement{
 
-      private Constant<?> constant;
-      private Constant<Number> limit;
-
-      public GreaterThan(Constant<Number> limit, Constant<?> constant) {
-         super("greaterThan", limit, constant);
-         this.limit = limit;
-         this.constant = constant;
-      }
-      
-      @Override
-      public String toString(){
-         return "(" + limit + " > " + constant + ")"; // constant > limit
-      }
-      
-      @Override
-      public boolean match(Statement other){
-         return super.match(other);
-      }
-      
-      @Override
-      public boolean isToBeEvaluated() {
-         return true;
-      }
-      
-      @Override
-      public boolean evaluate(){
-         if (!(constant.getValue() instanceof Number))
-            return false;
-         else
-            return ((Number) constant.getValue()).floatValue() > limit.getValue().floatValue(); 
-      }
-      
-      @Override
-      protected Statement createStatement(Constant<?>[] constants){
-         return new GreaterThan((Constant<Number>) constants[0], constants[1]);
-      }
-      
+   private boolean query(String function, String parameter) {
+      return kb.query(new Statement(function, new Constant<String>(parameter)));
    }
-   
+
 }
