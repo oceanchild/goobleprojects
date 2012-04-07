74e2efb7c41d0b9613bf9f0a679d42e29aba5dfb
gooble.sk <gooble.sk@gmail.com>
2012-04-07 20:41:13 +0000

    can also parse the greater than operator

--- GoobleLogic/src/test/kb/KBEncodingTest.java
+++ GoobleLogic/src/test/kb/KBEncodingTest.java
@@ -4,6 +4,7 @@ import main.kb.Constant;
 import main.kb.Rule;
 import main.kb.Statement;
 import main.kb.Variable;
+import main.kb.stmts.GreaterThan;
 
 import org.junit.Test;
 
@@ -14,14 +15,19 @@ public class KBEncodingTest {
    
    @Test
    public void encode_statement() throws Exception{
-      assertEquals(new Statement("p", new Variable("X")), stmt("p(X)"));
-      assertEquals(new Statement("p", new Variable("X"), new Variable("Y")), stmt("p(X, Y)"));
-      assertEquals(new Statement("p", new Constant<String>("a"), new Variable("Y")), stmt("p(a, Y)"));
+      assertEquals(new Statement("p", new Variable("X")), statement("p(X)"));
+      assertEquals(new Statement("p", new Variable("X"), new Variable("Y")), statement("p(X, Y)"));
+      assertEquals(new Statement("p", new Constant<String>("a"), new Variable("Y")), statement("p(a, Y)"));
    }
    
    @Test
    public void encode_rule() throws Exception{
       assertEquals(new Rule(new Statement("p", new Variable("X")), new Statement("g", new Variable("X"))), rule("g(X) => p(X)"));
       assertEquals(new Rule(new Statement("p", new Variable("X")), new Statement("g", new Variable("X")), new Statement("h", new Variable("X"))), rule("g(X) ^ h(X) => p(X)"));
+   }
+   
+   @Test
+   public void encode_rule_with_operator() throws Exception{
+      assertEquals(new Rule(new Statement("p", new Variable("X")), new GreaterThan(new Constant<Number>(17), new Variable("X"))), rule("X > 17 => p(X)"));
    }
 }
