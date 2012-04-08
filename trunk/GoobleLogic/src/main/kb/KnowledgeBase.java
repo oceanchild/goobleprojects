7c33904bf9ef67cf0669c98d965163843029968b
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-04-08 21:48:47 +0000

    refactoring of main algorithm

--- trunk/GoobleLogic/src/main/kb/KnowledgeBase.java
+++ trunk/GoobleLogic/src/main/kb/KnowledgeBase.java
@@ -17,69 +17,68 @@ public class KnowledgeBase {
       stmts.add(statement);
    }
 
-   public boolean query(Statement statement) {
-      return findSolutions(statement).isQueryTrue();
-   }
-
    public void add(Rule rule) {
       rules.add(rule);
    }
 
-   @Override
-   public String toString(){
-      return "STATEMENTS: " + stmts.toString() + "\n" + "RULES: " + rules.toString();
+   public boolean query(Statement statement) {
+      return findSolutions(statement).isQueryTrue();
    }
 
    public SolutionSet findSolutions(Statement statement) {
       List<Solution> solns = new ArrayList<Solution>();
-      
-      boolean queryWithNoVariablesTrue = checkIfMatchesStatement(statement, solns);
-      SolutionSet solution = new SolutionSet(solns, queryWithNoVariablesTrue);
-      if (queryWithNoVariablesTrue)
-         return solution;
+      if (matchesStatement(statement, solns))
+         return new SolutionSet(solns, true);
       if (statement.isToBeEvaluated()){
          return new SolutionSet(solns, statement.evaluate());
       }
+      return collectSolutionsUsingRules(statement);
+   }
+
+   private SolutionSet collectSolutionsUsingRules(Statement statement) {
+      SolutionSet solution = new SolutionSet(new ArrayList<Solution>(), false);
+      boolean atLeastOneRuleSucceeded = false;
       for (Rule rule : rules){
          if (rule.consequenceMatches(statement)){
-            SolutionSet subSolSet = new SolutionSet(new ArrayList<Solution>(), true);
-            
-            for (Statement currStmtToProve : rule.getAntecedents()){
-               List<Replacement> replacements = rule.getConsequence().unifyWith(statement);
-               if (!subSolSet.hasSolutions()){
-                  SolutionSet statementSolutions = findSolutions(currStmtToProve.applyReplacements(replacements));
-                  subSolSet.add(statementSolutions);
-               }else{
-                  SolutionSet newSubSolSet = new SolutionSet(new ArrayList<Solution>(), false);
-                  
-                  for (int i = subSolSet.size() - 1; i >= 0; i--){
-                     Solution currentSolution = subSolSet.get(i);
-                     Statement fullyUnifiedStatement = currStmtToProve.applyReplacements(replacements).applyReplacements(currentSolution.getReplacements());
-                     SolutionSet statementSolutions = findSolutions(fullyUnifiedStatement);
-                     
-                     if (!statementSolutions.isQueryTrue()){
-                        subSolSet.remove(i);
-                        i--;
-                     }else if (statementSolutions.hasSolutions()){
-                        for (Solution statementSol : statementSolutions.getSolutions()){
-                           newSubSolSet.add(currentSolution.mergeWith(statementSol));
-                        }
-                     }
-                  }
-                  subSolSet.replaceSolutionsIfNotEmpty(newSubSolSet);
-               }
-            }
-            
-            queryWithNoVariablesTrue |= subSolSet.isQueryTrue();
+            SolutionSet subSolSet = collectSolutionsForRule(statement, rule);
+            atLeastOneRuleSucceeded |= subSolSet.isQueryTrue();
             solution.add(subSolSet);
          }
       }
-
-      solution.setSucceeded(queryWithNoVariablesTrue);
+      solution.setSucceeded(atLeastOneRuleSucceeded);
       return solution;
    }
 
-   private boolean checkIfMatchesStatement(Statement statement, List<Solution> solns) {
+   private SolutionSet collectSolutionsForRule(Statement statement, Rule rule) {
+      SolutionSet subSolSet = new SolutionSet(new ArrayList<Solution>(), true);
+      for (Statement currStmtToProve : rule.getAntecedents()){
+         List<Replacement> replacements = rule.getConsequence().unifyWith(statement);
+         if (!subSolSet.hasSolutions()){
+            SolutionSet statementSolutions = findSolutions(currStmtToProve.applyReplacements(replacements));
+            subSolSet.add(statementSolutions);
+         }else{
+            SolutionSet newSubSolSet = new SolutionSet(new ArrayList<Solution>(), false);
+            for (int i = subSolSet.size() - 1; i >= 0; i--){
+               Solution currentSolution = subSolSet.get(i);
+               Statement fullyUnifiedStatement = currStmtToProve.applyReplacements(replacements).applyReplacements(currentSolution.getReplacements());
+               SolutionSet statementSolutions = findSolutions(fullyUnifiedStatement);
+
+               if (!statementSolutions.isQueryTrue()){
+                  subSolSet.remove(i);
+                  i--;
+               }else if (statementSolutions.hasSolutions()){
+                  for (Solution statementSol : statementSolutions.getSolutions()){
+                     newSubSolSet.add(currentSolution.mergeWith(statementSol));
+                  }
+               }
+            }
+            subSolSet.replaceSolutionsIfNotEmpty(newSubSolSet);
+         }
+      }
+      return subSolSet;
+   }
+
+   private boolean matchesStatement(Statement statement, List<Solution> solns) {
       boolean matchesStatement = false;
       for (Statement stmt : stmts){
          if (stmt.match(statement)){
@@ -97,4 +96,8 @@ public class KnowledgeBase {
       return matchesStatement;
    }
 
+   @Override
+   public String toString(){
+      return "STATEMENTS: " + stmts.toString() + "\n" + "RULES: " + rules.toString();
+   }
 }
