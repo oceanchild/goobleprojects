3353166bef8e973eb057d1b1ad9999a0beeef14b
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-04-15 02:38:48 +0000

    working towards finding the one true solution

--- trunk/GoobleLogic/src/com/gooble/logic/test/kb/ContradictingSolutionsTest.java
+++ trunk/GoobleLogic/src/com/gooble/logic/test/kb/ContradictingSolutionsTest.java
@@ -20,15 +20,89 @@ public class ContradictingSolutionsTest {
    public void remove_contradicting_solutions_for_unique_variable_value_domains() throws Exception{
       List<Solution> newSolutions = new ContradictingSolutions(
             rule("ageOf(bob, X) ^ ageOf(G, Y) ^ X < Y ^ wears(blue, G) => solution1(X, Y, G)"),
-            statement("solution1(AGEBOB, AGEOTHER, OTHER)"),
+            statement("solution1(BOBAGE, BLUESHOESAGE, BLUESHOESGUY)"),
             Arrays.asList(
-            solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "bob")),
-            solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "alex")),
-            solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "john")))).remove();
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "john")))).remove();
       
       assertEquals(Arrays.asList(
-            solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "alex")),
-            solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "john"))), newSolutions);
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "john"))), newSolutions);
+   }
+   
+   @Test
+   public void merged_solution_with_subset() throws Exception{
+      List<Solution> newSolutions = new ContradictingSolutions(
+            rule("ageOf(bob, X) ^ ageOf(G, Y) ^ X < Y ^ wears(blue, G) ^ ageOf(G1, A1) ^ ageOf(G2, A2) ^ A1 < A2 ^ wears(green, G2) ^ wears(red, G1) => solution1(X, Y, G, G1, G2, A1, A2)"),
+            statement("solution1(BOBAGE, BLUESHOESAGE, BLUESHOESGUY, REDSHOESYOUNGGUY, GREENSHOESOLDGUY, REDSHOESAGE, GREENSHOESAGE)"),
+            Arrays.asList(
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "bob"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "john"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "bob"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "john"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "bob"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "john"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")))).remove();
+      assertEquals(Arrays.asList(solution(replacement("BOBAGE", 22), replacement("BLUESHOESAGE", 33), replacement("BLUESHOESGUY", "alex"), replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob"))), 
+            newSolutions);
+   }
+   
+   @Test
+   public void remove_contradictory_solutions_case_2() throws Exception{
+      List<Solution> newSolutions = new ContradictingSolutions(
+            rule("ageOf(G1, A1) ^ ageOf(G2, A2) ^ A1 < A2 ^ wears(green, G2) ^ wears(red, G1) => solution2(G1, G2, A1, A2)"), 
+            statement("solution2(REDSHOESYOUNGGUY, GREENSHOESOLDGUY, REDSHOESAGE, GREENSHOESAGE)"), Arrays.asList(
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")), 
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john"))
+            )).remove();
+      assertEquals(Arrays.asList(
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 17), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 22), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "john"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "bob")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "alex"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john")),
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "alex")), 
+            solution(replacement("REDSHOESAGE", 22), replacement("REDSHOESYOUNGGUY", "bob"), replacement("GREENSHOESAGE", 33), replacement("GREENSHOESOLDGUY", "john"))
+            ), newSolutions);
    }
    
 }
--- trunk/GoobleLogic/src/com/gooble/logic/test/kb/SimpleCaseTest.java
+++ trunk/GoobleLogic/src/com/gooble/logic/test/kb/SimpleCaseTest.java
@@ -36,6 +36,14 @@ public class SimpleCaseTest {
       
       // bob is younger than the guy wearing blue shoes
       kb.add(rule("ageOf(bob, X) ^ ageOf(G, Y) ^ X < Y ^ wears(blue, G) => solution1(X, Y, G)"));
+
+      // the guy wearing green shoes is older than the guy wearing red shoes
+      kb.add(rule("ageOf(G1, A1) ^ ageOf(G2, A2) ^ A1 < A2 ^ wears(green, G2) ^ wears(red, G1) => solution2(G1, G2, A1, A2)"));
+      
+      // john wears red shoes
+      kb.add(statement("wears(red, john)"));
+      
+      // Solution: alex, 33, blue; john, 17, red; bob, 22, green
       
       assertEquals(Arrays.asList(
             solution(replacement("AGEBOB", 22), replacement("AGEOTHER", 33), replacement("OTHER", "bob")),
@@ -48,6 +56,38 @@ public class SimpleCaseTest {
             solution(replacement("AGEBOB", 17), replacement("AGEOTHER", 22), replacement("OTHER", "alex")),
             solution(replacement("AGEBOB", 17), replacement("AGEOTHER", 22), replacement("OTHER", "john"))),
             kb.findSolutions(statement("solution1(AGEBOB, AGEOTHER, OTHER)")).getSolutions());
+      
+      assertEquals(Arrays.asList(
+            // ageOf(john, 17) ^ ageOf(bob, 33) ^ wears(green, john) ^ wears(red, bob)   
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 22), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 22), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 22), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 22), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 22), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 22), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 22), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 22), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 17), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 22), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "john"), replacement("OLDAGE", 33), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "alex"), replacement("OLDAGE", 33), replacement("OLDGUY", "john")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "bob")),
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "alex")), 
+            solution(replacement("YOUNGAGE", 22), replacement("YOUNGGUY", "bob"), replacement("OLDAGE", 33), replacement("OLDGUY", "john"))
+            ), 
+            kb.findSolutions(statement("solution2(YOUNGGUY, OLDGUY, YOUNGAGE, OLDAGE)")).getSolutions());
    }
    
    @After
