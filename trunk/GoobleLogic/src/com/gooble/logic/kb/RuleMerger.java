de1879160471d44ff56930a44ed57dc1338b6a2e
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-04-22 15:56:04 +0000

    rule merger no longer static class; refactor it

--- trunk/GoobleLogic/src/com/gooble/logic/kb/RuleMerger.java
+++ trunk/GoobleLogic/src/com/gooble/logic/kb/RuleMerger.java
@@ -8,12 +8,36 @@ import com.gooble.logic.kb.stmts.Statement;
 import com.gooble.logic.kb.terms.Term;
 
 public class RuleMerger {
+   
+   private final Rule rule2;
+   private final Rule rule1;
 
-   public static Rule merge(Rule rule1, Rule rule2) {
-      String mergedConsequentName = rule1.getConsequence().getName() + rule2.getConsequence().getName();
-      Statement[] allAntecedents = new Statement[rule1.getAntecedents().length + rule2.getAntecedents().length];
+   public RuleMerger(Rule rule1, Rule rule2){
+      this.rule1 = rule1;
+      this.rule2 = rule2;
+   }
+
+   public Rule merge() {
+      return new Rule(mergeConsequent(), mergeAntecedents());
+   }
+
+   private Statement mergeConsequent() {
+      return new Statement(mergeConsequentName(), mergeConsequentTerms());
+   }
+
+   private String mergeConsequentName() {
+      return rule1.getConsequence().getName() + rule2.getConsequence().getName();
+   }
+
+   private Term<?>[] mergeConsequentTerms() {
       List<Term<?>> mergedConsequentTerms = new ArrayList<Term<?>>();
-      
+      mergedConsequentTerms.addAll(Arrays.asList(suffix(rule1.getConsequence().getTerms(), "1")));
+      mergedConsequentTerms.addAll(Arrays.asList(suffix(rule2.getConsequence().getTerms(), "2")));
+      return mergedConsequentTerms.toArray(new Term<?>[rule1.getConsequence().getTerms().length + rule2.getConsequence().getTerms().length]);
+   }
+
+   private Statement[] mergeAntecedents() {
+      Statement[] allAntecedents = new Statement[rule1.getAntecedents().length + rule2.getAntecedents().length];
       for (int i = 0; i < rule1.getAntecedents().length; i++){
          allAntecedents[i] = new Statement(rule1.getAntecedents()[i].getName(), suffix(rule1.getAntecedents()[i].getTerms(), "1"));
       }
@@ -21,14 +45,7 @@ public class RuleMerger {
          int j = i - rule1.getAntecedents().length;
          allAntecedents[i] = new Statement(rule2.getAntecedents()[j].getName(), suffix(rule2.getAntecedents()[j].getTerms(), "2"));
       }
-      mergedConsequentTerms.addAll(Arrays.asList(suffix(rule1.getConsequence().getTerms(), "1")));
-      mergedConsequentTerms.addAll(Arrays.asList(suffix(rule2.getConsequence().getTerms(), "2")));
-      
-      return new Rule(new Statement(mergedConsequentName,
-            mergedConsequentTerms
-                  .toArray(new Term<?>[rule1.getConsequence().getTerms().length
-                        + rule2.getConsequence().getTerms().length])),
-            allAntecedents);
+      return allAntecedents;
    }
 
    private static Term<?>[] suffix(Term<?>[] terms, String suffix) {
