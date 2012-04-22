30efbcd3f884d126a8c9f6f741e0cb8288fe1b31
gooble.sk <gooble.sk@gmail.com>
2012-04-22 16:05:05 +0000

    refactor solution suffixer to just "suffix" because now it takes rule merger's old responsibilty of suffixing terms as well.

---
+++ GoobleLogic/src/com/gooble/logic/kb/Suffix.java
@@ -0,0 +1,33 @@
+package com.gooble.logic.kb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gooble.logic.kb.solutions.Solution;
+import com.gooble.logic.kb.terms.Term;
+import com.gooble.logic.kb.terms.Variable;
+
+public class Suffix {
+
+   public static Term<?>[] terms(Term<?>[] terms, String suffix) {
+      Term<?>[] suffixedTerms = new Term<?>[terms.length];
+      for (int i = 0; i < terms.length; i++){
+         suffixedTerms[i] = terms[i].copyWithSuffix(suffix);
+      }
+      return suffixedTerms;
+   }
+   
+   public static List<Solution> solutions(List<Solution> oldSolns, String suffix){
+      List<Solution> newSolns = new ArrayList<Solution>();
+
+      for (Solution s : oldSolns){
+         Solution soln = new Solution();
+         for (Replacement re : s.getReplacements()){
+            soln.addVariableReplacement(new Replacement(new Variable(re.getVariable().getValue() + suffix), re.getValue()));
+         }
+         newSolns.add(soln);
+      }
+      return newSolns;
+   }
+
+}
---
+++ GoobleLogic/src/com/gooble/logic/test/kb/solutions/SuffixTest.java
@@ -0,0 +1,30 @@
+package com.gooble.logic.test.kb.solutions;
+
+import static com.gooble.logic.kb.KBEncoding.constant;
+import static com.gooble.logic.kb.KBEncoding.replacement;
+import static com.gooble.logic.kb.KBEncoding.solution;
+import static com.gooble.logic.kb.KBEncoding.variable;
+import static junit.framework.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.gooble.logic.kb.Suffix;
+import com.gooble.logic.kb.terms.Term;
+
+public class SuffixTest {
+   
+   @Test
+   public void suffix_solutions() throws Exception{
+      assertEquals(Arrays.asList(solution(replacement("X1", "bob"))), 
+            Suffix.solutions(Arrays.asList(solution(replacement("X", "bob"))), "1"));
+   }
+   @Test
+   public void suffix_terms() throws Exception{
+      Term<?>[] oldTerms = new Term<?>[]{ constant("bob"), variable("Y"), constant(49)};
+      Term<?>[] expectedTerms = new Term<?>[]{ constant("bob"), variable("Y1"), constant(49)};
+      assertEquals(Arrays.asList(expectedTerms), Arrays.asList(Suffix.terms(oldTerms, "1")));
+   }
+
+}
