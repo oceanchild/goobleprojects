1b96c5e3bf862cc8d4da4dfca69d72ba04352075
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-05-06 01:34:04 +0000

    some refactoring amongst hint parts & shared logic

--- trunk/GoobleLogic/src/com/gooble/logic/puzzle/HintPart.java
+++ trunk/GoobleLogic/src/com/gooble/logic/puzzle/HintPart.java
@@ -1,9 +1,17 @@
 package com.gooble.logic.puzzle;
 
+import static com.gooble.logic.kb.encoding.KBEncoding.statement;
+
+import com.gooble.logic.TwoObjectsEqual;
 import com.gooble.logic.kb.stmts.Statement;
 
-public interface HintPart{
-   
-   Statement toStatement();
-   
+public abstract class HintPart{
+   abstract public String toString();
+   public Statement toStatement() {
+      return statement(toString());
+   }
+   @Override
+   public boolean equals(Object other){
+      return TwoObjectsEqual.byReflection(this, other);
+   }
 }
