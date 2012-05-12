6d81be168893114608a72bd444187b373077c55f
gooble.sk <gooble.sk@gmail.com>
2012-05-12 18:18:21 +0000

    make expected order necessary in constructor

--- GoobleLogic/src/com/gooble/logic/test/puzzle/SolutionDefinitionTest.java
+++ GoobleLogic/src/com/gooble/logic/test/puzzle/SolutionDefinitionTest.java
@@ -12,8 +12,7 @@ import com.gooble.logic.puzzle.SolutionDefinition;
 public class SolutionDefinitionTest {
    @Test
    public void adding_solution_creates_list_of_statements_encoding_those_solutions_based_on_main_and_variable_names() throws Exception {
-      SolutionDefinition solDef = new SolutionDefinition();
-      solDef.setExpectedOrder("woman", "house", "hair");
+      SolutionDefinition solDef = new SolutionDefinition("woman", "house", "hair");
       solDef.add("adele", 2, "brown");
       
       assertEquals(Arrays.asList(
