b6ae1666b9a733d1ff020bcdc7326391920641f6
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-05-12 19:50:37 +0000

    add some solver test, which has very basic logic; need to implement a Merger now, which just makes use of consistent Merge but keeps track of things like the previously merged rule/soln and so forth

--- trunk/GoobleLogic/src/com/gooble/logic/kb/KnowledgeBaseFacade.java
+++ trunk/GoobleLogic/src/com/gooble/logic/kb/KnowledgeBaseFacade.java
@@ -1,10 +1,12 @@
 package com.gooble.logic.kb;
 
+import com.gooble.logic.kb.solutions.SolutionSet;
 import com.gooble.logic.kb.stmts.Statement;
 
 public interface KnowledgeBaseFacade {
 
    void add(Rule rule);
    void add(Statement statement);
+   SolutionSet findSolutions(Statement statement);
    
 }
