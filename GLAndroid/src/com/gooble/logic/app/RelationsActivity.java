9e7a556d3b0b743771a3faf2a3359b92ae5af3b8
gooble.sk <gooble.sk@gmail.com>
2012-07-15 21:03:18 +0000

    get started on relation layout

---
+++ GLAndroid/src/com/gooble/logic/app/RelationsActivity.java
@@ -0,0 +1,16 @@
+package com.gooble.logic.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelationsActivity extends Activity {
+
+   @Override
+   public void onCreate(Bundle bundle){
+      super.onCreate(bundle);
+      setContentView(R.layout.relations);
+      
+      //TODO: Load all relations in a list with edit & delete buttons next to them
+   }
+   
+}
--- GLAndroid/src/com/gooble/logic/app/util/TableLayoutRow.java
+++ GLAndroid/src/com/gooble/logic/app/util/TableLayoutRow.java
@@ -3,6 +3,7 @@ package com.gooble.logic.app.util;
 import com.gooble.logic.app.R;
 
 import android.content.Context;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -11,10 +12,9 @@ import android.widget.TextView;
 
 public class TableLayoutRow {
 
-   private static final LayoutParams WIDE_VIEW_LAYOUT = new TableRow.LayoutParams(
-         TableRow.LayoutParams.FILL_PARENT, 
-         TableRow.LayoutParams.WRAP_CONTENT);
-   
+   private static final LayoutParams NARROW_VIEW_LAYOUT = new TableRow.LayoutParams(
+         TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
    private final TableLayout layout;
    private final Context context;
 
@@ -22,26 +22,31 @@ public class TableLayoutRow {
       this.layout = layout;
       this.context = context;
    }
-   
-   public void addRowLabeledWithTextField(String label){
-      TableRow.LayoutParams lparams = WIDE_VIEW_LAYOUT;
-      
+
+   public void addRowLabeledWithTextField(String label) {
       TableRow nameValueRow = new TableRow(context);
-      nameValueRow.setLayoutParams(lparams);
-      
+      nameValueRow.setLayoutParams(new TableRow.LayoutParams(
+            TableRow.LayoutParams.FILL_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT));
+
       TextView name = new TextView(context);
       name.setText(label);
-      name.setLayoutParams(lparams);
-      
-      EditText edit = new EditText(context);
-      edit.setLayoutParams(lparams);
-      
-      nameValueRow.addView(name, WIDE_VIEW_LAYOUT);
-      nameValueRow.addView(edit, new TableRow.LayoutParams(
-            TableRow.LayoutParams.FILL_PARENT, 
+      name.setLayoutParams(NARROW_VIEW_LAYOUT);
+
+      EditText value = new EditText(context);
+      value.setLayoutParams(new TableRow.LayoutParams(
+            TableRow.LayoutParams.FILL_PARENT,
             TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
-      
-      layout.addView(nameValueRow, WIDE_VIEW_LAYOUT);
+
+      Button delete = new Button(context);
+      delete.setText(context.getString(R.string.delete));
+      delete.setLayoutParams(NARROW_VIEW_LAYOUT);
+
+      nameValueRow.addView(name);
+      nameValueRow.addView(value);
+      nameValueRow.addView(delete);
+
+      layout.addView(nameValueRow);
    }
-   
+
 }
