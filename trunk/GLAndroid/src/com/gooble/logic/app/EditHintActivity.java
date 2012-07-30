203e63c1aeff8eb61a2b9c712187b8d05ec89acc
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-07-30 22:02:51 +0000

    edit hint screen begun

---
+++ trunk/GLAndroid/src/com/gooble/logic/app/EditHintActivity.java
@@ -0,0 +1,14 @@
+package com.gooble.logic.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditHintActivity extends Activity {
+
+   @Override
+   public void onCreate(Bundle bundle){
+      super.onCreate(bundle);
+      setContentView(R.layout.edit_hint);
+   }
+   
+}
