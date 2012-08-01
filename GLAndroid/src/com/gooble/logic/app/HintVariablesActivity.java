3dec22d070d2779311f92b8e5cde47651b3f461b
gooble.sk <gooble.sk@gmail.com>
2012-08-01 15:44:09 +0000

    sub screens for editing hints

---
+++ GLAndroid/src/com/gooble/logic/app/HintVariablesActivity.java
@@ -0,0 +1,12 @@
+package com.gooble.logic.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HintVariablesActivity extends Activity {
+   @Override
+   public void onCreate(Bundle bundle){
+      super.onCreate(bundle);
+      setContentView(R.layout.hint_variables);
+   }
+}
