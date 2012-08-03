228338e2c4b460e6cc3da1af3046c63f5e26c58a
gooble.sk <gooble.sk@gmail.com>
2012-08-03 16:25:05 +0000

    revert to old table layout

--- GLAndroid/src/com/gooble/logic/app/VariablesActivity.java
+++ GLAndroid/src/com/gooble/logic/app/VariablesActivity.java
@@ -1,23 +1,17 @@
 package com.gooble.logic.app;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.gooble.logic.app.api.VariableDomain;
 import com.gooble.logic.app.api.VariableFacade;
+import com.gooble.logic.app.util.TableLayoutRow;
 
 public class VariablesActivity extends Activity {
    
@@ -25,72 +19,6 @@ public class VariablesActivity extends Activity {
 
    public VariablesActivity() {
       this.variableFacade = new VariableDomain();
-   }
-   
-   public class Variable{
-
-      private final String name;
-
-      public Variable(String name) {
-         this.name = name;
-      }
-
-      public String getName() {
-         return name;
-      }
-      
-   }
-   public class VariableHolder{
-
-      public EditText name;
-      
-   }
-   
-   public class VariableAdapter extends ArrayAdapter<Variable>{
-
-      private final int layoutResourceId;
-
-      public VariableAdapter(Context context, int layoutResourceId, List<Variable> variables) {
-         super(context, layoutResourceId, variables);
-         this.layoutResourceId = layoutResourceId;
-      }
-      
-      @Override
-      public View getView(final int position, View convertView, ViewGroup parent){
-         View row = convertView;
-         
-         VariableHolder holder = null;
-         if (row == null){
-            LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            
-            holder = new VariableHolder();
-            holder.name = (EditText) row.findViewById(R.id.variable_name);
-            Button addButton = (Button) row.findViewById(R.id.add_values_button);
-            addButton.setOnClickListener(new OnClickListener() {
-               public void onClick(View v) {
-                  variableFacade.save();
-                  startActivity(new Intent(getContext(), VariableValuesActivity.class));
-               }
-            });
-            Button deleteButton = (Button) row.findViewById(R.id.delete_variable_button);
-            deleteButton.setOnClickListener(new OnClickListener() {
-               public void onClick(View v) {
-                  remove(getItem(position));
-               }
-            });
-            row.setTag(holder);
-            
-         }else{
-            holder = (VariableHolder) row.getTag();
-         }
-         
-         Variable variable = getItem(position);
-         holder.name.setText(variable.getName());
-         
-         return row;
-      }
-      
    }
    
    @Override
@@ -104,28 +32,49 @@ public class VariablesActivity extends Activity {
        *       or maybe this should go on the variable screen.
        */
       
-      final ListView variableList = (ListView)findViewById(R.id.variable_list);
-      final List<Variable> variables = new ArrayList<Variable>();
-      variables.add(new Variable("X"));
-      variables.add(new Variable("Y"));
-      final VariableAdapter adapter = new VariableAdapter(this, R.layout.variable_row, variables);
-      variableList.setAdapter(adapter);
-      
+      final Activity activity = this;
       Button addVariableButton = (Button) findViewById(R.id.add_variable_button);
-      addVariableButton.setOnClickListener(new OnClickListener() {
-         public void onClick(View v) {
-            variables.add(new Variable(""));
-            adapter.notifyDataSetChanged();
-         }
-      });
-      
       Button saveVariablesButton = (Button) findViewById(R.id.save_variables_button);
       saveVariablesButton.setOnClickListener(new OnClickListener() {
          public void onClick(View v) {
-            variableFacade.save(); //TODO: need to pass in data
+            variableFacade.save();
             // TODO: maybe go back to previous activity here? Finish?
          }
       });
       
+      addVariableButton.setOnClickListener(new OnClickListener() {
+         public void onClick(View v) {
+            TableLayout layout = (TableLayout) findViewById(R.id.variable_layout);
+            new TableLayoutRow(layout, activity).addRowLabeledWithTextField(getString(R.string.variable_name));
+            TableRow.LayoutParams lparams = new TableRow.LayoutParams(
+                  TableRow.LayoutParams.FILL_PARENT, 
+                  TableRow.LayoutParams.WRAP_CONTENT);
+            
+            TableRow addValuesRow = new TableRow(activity);
+            addValuesRow.setLayoutParams(lparams);
+            
+            /*
+             * TODO: Onclick for this button should go to the Add Variable Values activity
+             * set the clicked variable as the current variable that's being edited
+             * 
+             * it will also have to do the same thing as the "save" button
+             *  (i.e save to DB all the variables for this puzzle)
+             */
+            Button addVariableValuesButton = new Button(activity);
+            addVariableValuesButton.setText(getString(R.string.add_variable_values));
+            addVariableValuesButton.setOnClickListener(new OnClickListener() {
+               public void onClick(View v) {
+                  variableFacade.save();
+                  activity.startActivity(new Intent(activity, VariableValuesActivity.class));
+               }
+            });
+            addValuesRow.addView(addVariableValuesButton, new TableRow.LayoutParams(
+                  TableRow.LayoutParams.WRAP_CONTENT, 
+                  TableRow.LayoutParams.WRAP_CONTENT));
+            layout.addView(addValuesRow, new TableRow.LayoutParams(
+                  TableRow.LayoutParams.FILL_PARENT, 
+                  TableRow.LayoutParams.WRAP_CONTENT));
+         }
+      });
    }
 }
