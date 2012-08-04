b7a01ba7145f2cc0e3a2191519c3393bfccf8cf8
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-08-04 19:17:49 +0000

    convert relation layout to use inflater & get rid of tablelayoutrow

--- trunk/GLAndroid/src/com/gooble/logic/app/RelationsActivity.java
+++ trunk/GLAndroid/src/com/gooble/logic/app/RelationsActivity.java
@@ -1,15 +1,13 @@
 package com.gooble.logic.app;
 
-import com.gooble.logic.app.util.TableLayoutRow;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 
 public class RelationsActivity extends Activity {
 
@@ -21,36 +19,28 @@ public class RelationsActivity extends Activity {
       //TODO: Load all relations in a list with edit & delete buttons next to them
       
       final Activity activity = this;
+      final LinearLayout layout = (LinearLayout) findViewById(R.id.relation_container);
       Button addRelationButton = (Button) findViewById(R.id.add_relation_button);
       
       addRelationButton.setOnClickListener(new OnClickListener() {
          public void onClick(View v) {
-            TableLayout layout = (TableLayout) findViewById(R.id.relations_layout);
-            new TableLayoutRow(layout, activity).addRowLabeledWithTextField(getString(R.string.relation_name));
-            TableRow.LayoutParams lparams = new TableRow.LayoutParams(
-                  TableRow.LayoutParams.FILL_PARENT, 
-                  TableRow.LayoutParams.WRAP_CONTENT);
+            LayoutInflater inflater = getLayoutInflater();
+            final View relation = inflater.inflate(R.layout.relation_row, layout, false);
             
-            TableRow addValuesRow = new TableRow(activity);
-            addValuesRow.setLayoutParams(lparams);
-            
-            /*
-             * TODO: Reduce duplication between this activity & variables activity
-             * 
-             */
-            Button addVariableValuesButton = new Button(activity);
-            addVariableValuesButton.setText(getString(R.string.edit_relation));
-            addVariableValuesButton.setOnClickListener(new OnClickListener() {
+            Button addRelationButton = (Button) relation.findViewById(R.id.edit_relation_button);
+            addRelationButton.setOnClickListener(new OnClickListener() {
                public void onClick(View v) {
                   activity.startActivity(new Intent(activity, EditRelationActivity.class));
                }
             });
-            addValuesRow.addView(addVariableValuesButton, new TableRow.LayoutParams(
-                  TableRow.LayoutParams.WRAP_CONTENT, 
-                  TableRow.LayoutParams.WRAP_CONTENT));
-            layout.addView(addValuesRow, new TableRow.LayoutParams(
-                  TableRow.LayoutParams.FILL_PARENT, 
-                  TableRow.LayoutParams.WRAP_CONTENT));
+            Button deleteButton = (Button) relation.findViewById(R.id.delete_relation_button);
+            deleteButton.setOnClickListener(new OnClickListener() {
+               public void onClick(View v) {
+                  layout.removeView(relation);
+               }
+            });
+            
+            layout.addView(relation);
          }
       });
    }
