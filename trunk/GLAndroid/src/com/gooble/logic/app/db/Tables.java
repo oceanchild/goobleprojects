7c030131b76643a2678d4791eb738aeea4cffcfa
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-08-05 18:19:25 +0000

    update to the model; needed to differentiate between instances of relations (i.e for hints) and definitions of relations (for the puzzle on the whole; to be _used_ for hints)

--- trunk/GLAndroid/src/com/gooble/logic/app/db/Tables.java
+++ trunk/GLAndroid/src/com/gooble/logic/app/db/Tables.java
@@ -8,10 +8,6 @@ public class Tables {
       public static final String NAME = "name";
       public static final String MAINVARIABLEID = "mainvariableid";
    }
-   
-   public class Hint implements BaseColumns{
-      public static final String PUZZLEID = "puzzleid";
-   }
 
    public class Variable implements BaseColumns{
       public static final String NAME = "name";
@@ -22,13 +18,42 @@ public class Tables {
       public static final String VARIABLEID = "variableid";
    }
    public class Term implements BaseColumns{
-      public static final String VALUE = "value";
+      public static final String VALUE = "value"; // if it's a variable it will have this eg X Y Z
       public static final String VARIABLE = "variable"; // whether or not this term is a variable
-      public static final String VARIABLEID = "variableid"; 
+      public static final String VARIABLEID = "variableid";
+      public static final String VARIABLEVALUEID = "variablevalueid"; // if it's not a variable it will have this instead
    }
    
    /*
-    * relation definitions, not for hints 
+    * Hints
+    */
+   public class Hint implements BaseColumns{
+      public static final String PUZZLEID = "puzzleid";
+   }
+   public class Hintvariable implements BaseColumns{
+      public static final String NAME = "name"; // like X Y Z
+      public static final String HINTID = "hintid";
+      public static final String VARIABLEID = "variableid";
+   }
+   public class Hintrelation implements BaseColumns{
+      public static final String HINTID = "hintid";
+      public static final String RELATIONID = "relationid";
+   }
+   public class Hintrelationterm implements BaseColumns{
+      public static final String HINTRELATIONID = "hintrelationid";
+      public static final String VARIABLEVALUEID = "variablevalueid"; // has this
+      public static final String HINTVARIABLEID = "hintvariableid"; // or has this. not both though, and not neither
+   }
+   public class Property implements BaseColumns{
+      public static final String HINTID = "hintid";
+   }
+   public class Propertyterm implements BaseColumns{
+      public static final String PROPERTYID = "propertyid";
+      public static final String HINTVARIABLEID = "hintvariableid";
+   }
+   
+   /*
+    * relation definitions (for the overall puzzle; not specific instances for hints) 
     */
    public class Relation implements BaseColumns{
       public static final String NAME = "name";
@@ -41,21 +66,12 @@ public class Tables {
       public static final String TERMID = "termid";
       public static final String RELATIONORDER = "relationorder"; // the order that this variable comes in in the relation. eg for rel(X, Y) , RELATIONORDER(X) must be less than RELATIONORDER(Y)
    }
-
    public class Condition implements BaseColumns{
       public static final String RELATIONID = "relationid";
       public static final String OPERATOR = "operator";
    }
    public class Conditionterm implements BaseColumns{
       public static final String CONDITIONID = "conditionid";
-      public static final String TERMID = "termid";
-   }
-   
-   public class Property implements BaseColumns{
-      public static final String HINTID = "hintid";
-   }
-   public class Propertyterm implements BaseColumns{
-      public static final String PROPERTYID = "propertyid";
       public static final String TERMID = "termid";
    }
    
