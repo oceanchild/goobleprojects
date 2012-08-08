271bd1173a9a7c22882c79a4a824dea9e130a377
gooble.sk <gooble.sk@gmail.com>
2012-08-08 18:12:16 +0000

    create database helper + drop table sql generator

--- GLAndroidUTests/src/com/gooble/logic/test/app/db/sql/CreateTableSqlUTest.java
+++ GLAndroidUTests/src/com/gooble/logic/test/app/db/sql/CreateTableSqlUTest.java
@@ -6,26 +6,17 @@ import org.junit.Test;
 
 import com.gooble.logic.app.db.Tables;
 import com.gooble.logic.app.db.sql.CreateTableSql;
-import com.gooble.logic.app.entity.Entity;
 
 public class CreateTableSqlUTest {
 
-   @SuppressWarnings("unused")
-   class Testentity extends Entity{
-      private String name;
-      private int number;
-      private double money;
-   }
-   
    @Test
    public void generate_sql_for_entity() {
-      Testentity testentity = new Testentity();
       assertEquals("CREATE TABLE testentity (" +
             Tables._ID + " INTEGER PRIMARY KEY, " +
       		"name TEXT, " +
       		"number INTEGER, " +
       		"money REAL" +
-      		");", new CreateTableSql().generateFor(testentity));
+      		");", new CreateTableSql().generateFor(Testentity.class));
    }
    
 }
---
+++ GLAndroidUTests/src/com/gooble/logic/test/app/db/sql/DropTableSqlUTest.java
@@ -0,0 +1,16 @@
+package com.gooble.logic.test.app.db.sql;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.gooble.logic.app.db.sql.DropTableSql;
+
+public class DropTableSqlUTest {
+
+   @Test
+   public void drop_table_sql() {
+      assertEquals("DROP TABLE IF EXISTS testentity;", new DropTableSql().generateFor(Testentity.class));
+   }
+   
+}
