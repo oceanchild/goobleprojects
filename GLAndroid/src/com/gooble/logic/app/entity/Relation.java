45546abca6e558036b3e5ebd72a04e2fcb850106
gooble.sk <gooble.sk@gmail.com>
2012-08-09 00:26:10 +0000

    use boolean type in entity

--- GLAndroid/src/com/gooble/logic/app/entity/Relation.java
+++ GLAndroid/src/com/gooble/logic/app/entity/Relation.java
@@ -3,31 +3,31 @@ package com.gooble.logic.app.entity;
 public class Relation extends Entity{
 
    private String name;
-   private Integer bidirectional;
-   private Integer uniquevalue;
-   private Integer variableid;
+   private Boolean bidirectional;
+   private Boolean uniquevalue;
+   private Boolean variableid;
    public String getName() {
       return name;
    }
    public void setName(String name) {
       this.name = name;
    }
-   public Integer getBidirectional() {
+   public Boolean getBidirectional() {
       return bidirectional;
    }
-   public void setBidirectional(Integer bidirectional) {
+   public void setBidirectional(Boolean bidirectional) {
       this.bidirectional = bidirectional;
    }
-   public Integer getUniquevalue() {
+   public Boolean getUniquevalue() {
       return uniquevalue;
    }
-   public void setUniquevalue(Integer uniquevalue) {
+   public void setUniquevalue(Boolean uniquevalue) {
       this.uniquevalue = uniquevalue;
    }
-   public Integer getVariableid() {
+   public Boolean getVariableid() {
       return variableid;
    }
-   public void setVariableid(Integer variableid) {
+   public void setVariableid(Boolean variableid) {
       this.variableid = variableid;
    }
 }
--- GLAndroidUTests/src/com/gooble/logic/test/app/entity/EntityUTest.java
+++ GLAndroidUTests/src/com/gooble/logic/test/app/entity/EntityUTest.java
@@ -1,6 +1,7 @@
 package com.gooble.logic.test.app.entity;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -9,12 +10,21 @@ public class EntityUTest {
    @Test
    public void set_field() throws Exception{
       Testentity e = new Testentity();
+      
+      assertNull(e.getName());
+      assertNull(e.getMoney());
+      assertNull(e.getNumber());
+      assertNull(e.getCool());
+      
       e.setField("name", "Boom");
       e.setField("money", 6.33);
       e.setField("number", 7);
+      e.setField("cool", false);
+      
       assertEquals("Boom", e.getName());
       assertEquals(6.33, e.getMoney(), 0.00001);
       assertEquals(Integer.valueOf(7), e.getNumber());
+      assertEquals(Boolean.FALSE, e.getCool());
    }
    
 }
