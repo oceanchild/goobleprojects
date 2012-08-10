0ae8c97aa9911119d0e002832820c50eed4ca2d2
gooble.sk <gooble.sk@gmail.com>
2012-08-10 15:58:42 +0000

    add Type column to variable (which can be either TEXT or NUMBER)

--- GLAndroid/src/com/gooble/logic/app/entity/Variable.java
+++ GLAndroid/src/com/gooble/logic/app/entity/Variable.java
@@ -1,21 +1,33 @@
 package com.gooble.logic.app.entity;
 
-public class Variable extends Entity{
+public class Variable extends Entity {
 
    private String name;
+   private String type;
    private Long puzzleid;
-   
+
    public String getName() {
       return name;
    }
+
    public void setName(String name) {
       this.name = name;
    }
+
    public Long getPuzzleid() {
       return puzzleid;
    }
+
    public void setPuzzleid(Long puzzleid) {
       this.puzzleid = puzzleid;
    }
-   
+
+   public String getType() {
+      return type;
+   }
+
+   public void setType(String type) {
+      this.type = type;
+   }
+
 }
