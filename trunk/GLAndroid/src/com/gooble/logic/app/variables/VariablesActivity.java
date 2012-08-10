4f43a37453e7f7d40a78478eab72fa167603f3f4
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-08-10 17:35:23 +0000

    minor fixes

--- trunk/GLAndroid/src/com/gooble/logic/app/variables/VariablesActivity.java
+++ trunk/GLAndroid/src/com/gooble/logic/app/variables/VariablesActivity.java
@@ -3,7 +3,6 @@ package com.gooble.logic.app.variables;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
