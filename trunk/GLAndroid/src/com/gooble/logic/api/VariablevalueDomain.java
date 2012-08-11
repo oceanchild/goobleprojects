376e12b4eea4a732befc67e197cce617495e4521
gooble.sk@gmail.com <gooble.sk@gmail.com@684f78b6-ac73-fb25-fb8e-6ee683b1feb4>
2012-08-11 17:01:02 +0000

    save variable type

--- trunk/GLAndroid/src/com/gooble/logic/api/VariablevalueDomain.java
+++ trunk/GLAndroid/src/com/gooble/logic/api/VariablevalueDomain.java
@@ -7,11 +7,18 @@ import android.content.Context;
 import com.gooble.logic.api.domain.FieldMap;
 import com.gooble.logic.api.domain.FieldMapper;
 import com.gooble.logic.app.db.Tables;
+import com.gooble.logic.app.db.entity.VariableAdapter;
+import com.gooble.logic.app.entity.Variable;
 import com.gooble.logic.app.variables.VariablevalueAdapter;
 
 public class VariablevalueDomain implements VariablevalueFacade{
 
-   public List<Long> save(Context context, Long variableId, List<Long> ids, List<String> values) {
+   public List<Long> save(Context context, Long variableId, String variableType, List<Long> ids, List<String> values) {
+      VariableAdapter varHelper = new VariableAdapter(context);
+      Variable variable = varHelper.getById(variableId);
+      variable.setType(variableType);
+      varHelper.store(variable);
+      
       VariablevalueAdapter helper = new VariablevalueAdapter(context);
       
       FieldMap valueMap = new FieldMap();
