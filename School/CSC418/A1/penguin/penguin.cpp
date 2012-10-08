0aceba4140776445a99cdfb4cb5d3c5a4e0bd907
gooble.sk <gooble.sk@gmail.com>
2012-10-08 20:13:54 +0000

    make body bigger, draw head + beak

--- School/CSC418/A1/penguin/penguin.cpp
+++ School/CSC418/A1/penguin/penguin.cpp
@@ -129,9 +129,9 @@ int main(int argc, char** argv)
     // Process program arguments
     if(argc != 3) {
         printf("Usage: demo [width] [height]\n");
-        printf("Using 400x300 window by default...\n");
-        Win[0] = 400;
-        Win[1] = 300;
+        printf("Using 800x600 window by default...\n");
+        Win[0] = 800;
+        Win[1] = 600;
     } else {
         Win[0] = atoi(argv[1]);
         Win[1] = atoi(argv[2]);
@@ -325,17 +325,26 @@ void display(void)
     //   render the individual body parts.
     ///////////////////////////////////////////////////////////
 
+    /**
+     * what needs to be done still?
+     * - draw the eye
+     * - move beak up and down
+     * - draw circles representing joints
+     * - rotate joints
+     * - make spinners for each joint
+     */
+
     // Draw our hinged object
-    const float BODY_WIDTH = 70.0f;
-    const float BODY_LENGTH = 100.0f;
+    const float BODY_WIDTH = 140.0f;
+    const float BODY_LENGTH = 200.0f;
 
     // Push the current transformation matrix on the stack
     glPushMatrix();
         
-        // Draw the 'body'
+        // Draw the body
         glPushMatrix();
         {
-            // Scale square to size of body
+            // Scale to size of body
             glScalef(BODY_WIDTH, BODY_LENGTH, 1.0);
 
             // Set the colour to green
@@ -357,12 +366,44 @@ void display(void)
                 glColor3f(0.0, 0.0, 1.0);
                 Point armPoints[] = {{-0.4f, 0.5f}, {0.4f, 0.5f}, {0.3f, -0.5f}, {-0.3f, -0.5f}};
                 drawPolygon(4, armPoints);
-                //drawSquare(1.0);
             }
             glPopMatrix();
 
             drawLeg(1.0, 0);
             drawLeg(-1.0, 1);
+
+            // draw head
+            glPushMatrix();{
+                glTranslatef(0.0, 0.6, 0.0);
+                glScalef(0.6, 0.25, 1.0);
+                glColor3f(0.75, 0.75, 0.75);
+
+                Point headPoints[] = {{-0.1, 0.7}, {0.4, 0.4}, {0.5, -0.5}, {-0.5, -0.5}, {-0.4, 0.4}};
+                drawPolygon(5, headPoints);
+
+                // draw top beak
+                glPushMatrix();{
+                    glScalef(0.8, 0.4, 1.0);
+                    glTranslatef(-1.0, 0.0, 0.0);
+                    glColor3f(0.5, 0.5, 1.0);
+
+                    Point topBeakPoints[] = {{-0.5, 0.1}, {-0.5, -0.1}, {0.5, -0.1}, {0.5, 0.3}};
+                    drawPolygon(4, topBeakPoints);
+                }
+                glPopMatrix();
+
+                // draw bottom beak
+                glPushMatrix();{
+                    glScalef(0.8, 0.2, 1.0);
+                    glTranslatef(-1.0, -0.5, 0.0);
+                    glColor3f(0.5, 0.1, 1.0);
+
+                    Point bottomBeakPoints[] = {{-0.5, 0.1}, {-0.5, -0.1}, {0.5, -0.1}, {0.5, 0.1}};
+                    drawPolygon(4, bottomBeakPoints);
+                }
+                glPopMatrix();
+            }
+            glPopMatrix();
         }
         glPopMatrix();
 
@@ -399,16 +440,28 @@ void drawPolygon(int n, Point points[]){
 
 }
 
+float LEG_OFFSET_X = 1.0f/5.0f;
+float LEG_OFFSET_Y = -0.5;
+
+float LEG_WIDTH_REL_TO_BODY = 0.2f;
+float LEG_LENGTH_REL_TO_BODY = 0.3f;
+
+float FOOT_OFFSET_X = -0.5f;
+float FOOT_OFFSET_Y = -0.5f;
+
+float FOOT_WIDTH_REL_TO_LEG = 2.0f;
+float FOOT_LENGTH_REL_TO_LEG = 1.0f/3.0f;
+
 void drawLeg(float leftOrRight, int rotationIndex){
     // Draw a leg
     glPushMatrix();
     {
         // Move the leg to the joint hinge
-        glTranslatef(leftOrRight * 1.0/3.0, -0.5, 0.0);
+        glTranslatef(leftOrRight * LEG_OFFSET_X, LEG_OFFSET_Y, 0.0);
         // Rotate along the hinge
         glRotatef(joint_rot[rotationIndex], 0.0, 0.0, 1.0);
         // Scale the size of the leg
-        glScalef(0.2, 0.5, 1.0);
+        glScalef(LEG_WIDTH_REL_TO_BODY, LEG_LENGTH_REL_TO_BODY, 1.0);
         // Move to center location of leg, under previous rotation
         glTranslatef(0.0, -0.5, 0.0);
         // Draw the square for the leg
@@ -418,8 +471,8 @@ void drawLeg(float leftOrRight, int rotationIndex){
         // Draw the foot
         glPushMatrix();
         {
-            glTranslatef(-1.0, -0.5, 0.0);
-            glScalef(3.0, 1.0/3.0, 1.0);
+            glTranslatef(FOOT_OFFSET_X, FOOT_OFFSET_Y, 0.0);
+            glScalef(FOOT_WIDTH_REL_TO_LEG, FOOT_LENGTH_REL_TO_LEG, 1.0);
             glColor3f(1.0, 1.0, 0.0);
             drawSquare(1.0);
         }
