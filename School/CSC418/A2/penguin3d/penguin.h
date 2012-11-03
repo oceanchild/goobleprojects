966cc649d5a45740f0e3d3785ee391e3eda52836
gooble.sk <gooble.sk@gmail.com>
2012-11-03 16:06:31 +0000

    begin lighting stuff, but the penguin is too bright for now

--- School/CSC418/A2/penguin3d/penguin.h
+++ School/CSC418/A2/penguin3d/penguin.h
@@ -8,49 +8,49 @@
 #ifndef PENGUIN_H_
 #define PENGUIN_H_
 
-float ARM_WIDTH_TOP = 1.2f;
-float ARM_WIDTH_BOTTOM = 1.0f;
-float ARM_HEIGHT = 2.0f;
-float ARM_THICKNESS = 0.2f;
+const float ARM_WIDTH_TOP = 1.2f;
+const float ARM_WIDTH_BOTTOM = 1.0f;
+const float ARM_HEIGHT = 2.0f;
+const float ARM_THICKNESS = 0.2f;
 
-float HAND_WIDTH_TOP = 1.0f;
-float HAND_WIDTH_BOTTOM = 0.8f;
-float HAND_THICKNESS = ARM_THICKNESS;
-float HAND_HEIGHT = 0.7f;
+const float HAND_WIDTH_TOP = 1.0f;
+const float HAND_WIDTH_BOTTOM = 0.8f;
+const float HAND_THICKNESS = ARM_THICKNESS;
+const float HAND_HEIGHT = 0.7f;
 
-float LEG_WIDTH = 0.2f;
-float LEG_THICKNESS = 0.2f;
-float LEG_HEIGHT = 1.0f;
+const float LEG_WIDTH = 0.2f;
+const float LEG_THICKNESS = 0.2f;
+const float LEG_HEIGHT = 1.0f;
 
-float FOOT_THICKNESS = 0.1f;
-float FOOT_WIDTH = 0.5f;
-float FOOT_LENGTH = 0.5f;
+const float FOOT_THICKNESS = 0.1f;
+const float FOOT_WIDTH = 0.5f;
+const float FOOT_LENGTH = 0.5f;
 
-float HEAD_WIDTH_BOTTOM = 2.2f;
-float HEAD_WIDTH_TOP = 1.8f;
-float HEAD_HEIGHT = 2.0f;
-float HEAD_SHORTENER = 0.5f;
+const float HEAD_WIDTH_BOTTOM = 2.2f;
+const float HEAD_WIDTH_TOP = 1.8f;
+const float HEAD_HEIGHT = 2.0f;
+const float HEAD_SHORTENER = 0.5f;
 
-float BODY_WIDTH_TOP = 2.0f;
-float BODY_WIDTH_BOTTOM = 3.0f;
-float BODY_HEIGHT = 3.0f;
-float BODY_THICKNESS = 2.0f;
+const float BODY_WIDTH_TOP = 2.0f;
+const float BODY_WIDTH_BOTTOM = 3.0f;
+const float BODY_HEIGHT = 3.0f;
+const float BODY_THICKNESS = 2.0f;
 
-float BEAK_OFFSET_Y = 0.3f;
-float BEAK_OUTSIDE_WIDTH = 1.0f;
-float BEAK_INSIDE_WIDTH = 1.2f;
-float BEAK_THICKNESS = 0.2f;
-float BEAK_LENGTH = 0.8f;
+const float BEAK_OFFSET_Y = 0.3f;
+const float BEAK_OUTSIDE_WIDTH = 1.0f;
+const float BEAK_INSIDE_WIDTH = 1.2f;
+const float BEAK_THICKNESS = 0.2f;
+const float BEAK_LENGTH = 0.8f;
 
-float RED[] = {1.0, 0.0, 0.0};
-float GOLD[] = {1.0, 0.8, 0.0};
-float ORANGE[] = {1.0, 0.5, 0.0};
-float YELLOW[] = {1.0, 1.0, 0.0};
-float GREEN[] = {0.0, 1.0, 0.0};
-float CYAN[] = {0.0, 1.0, 1.0};
-float BLUE[] = {0.0, 0.0, 1.0};
-float PURPLE[] = {0.5, 0.0, 1.0};
-float MAGENTA[] = {1.0, 0.0, 1.0};
-float SILVER[] = {0.7, 0.7, 0.7};
+const float RED[] = {1.0, 0.0, 0.0};
+const float GOLD[] = {1.0, 0.8, 0.0};
+const float ORANGE[] = {1.0, 0.5, 0.0};
+const float YELLOW[] = {1.0, 1.0, 0.0};
+const float GREEN[] = {0.0, 1.0, 0.0};
+const float CYAN[] = {0.0, 1.0, 1.0};
+const float BLUE[] = {0.0, 0.0, 1.0};
+const float PURPLE[] = {0.5, 0.0, 1.0};
+const float MAGENTA[] = {1.0, 0.0, 1.0};
+const float SILVER[] = {0.7, 0.7, 0.7};
 
 #endif /* PENGUIN_H_ */
